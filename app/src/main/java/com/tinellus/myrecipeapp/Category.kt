3ce3cef99d1a9760e4015abab9adb5c150840c47
gil-fullstack <gilservicosweb@gmail.com>
2024-08-02 12:43:49 -0300

    created project

---
+++ app/src/main/java/com/tinellus/myrecipeapp/Category.kt
@@ -0,0 +1,10 @@
+package com.tinellus.myrecipeapp
+
+data class Category(
+    var idCategory: String,
+    var strCategory: String,
+    var strCategoryThumb: String,
+    var strCategoryDescription: String
+)
+
+data class CategoriesResponse(val categories: List<Category>)
---
+++ app/src/main/java/com/tinellus/myrecipeapp/RecipeScreen.kt
@@ -0,0 +1,10 @@
+package com.tinellus.myrecipeapp
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+
+@Composable
+fun RecipeScreen(modifier: Modifier = Modifier){
+    val recipeViewModel: MainViewModel = viewModel( )
+}
